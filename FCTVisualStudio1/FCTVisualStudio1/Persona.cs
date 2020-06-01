4ed24524ee3655e894fe4bfc8401a145ed9dbed9
Nuria Majano Santiago <alumno.113633@iesazarquiel.es>
2020-06-02 00:35:03 +0200

    Agregar clase Persona

---
+++ FCTVisualStudio1/FCTVisualStudio1/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCTVisualStudio1
+{
+    class Persona
+    {
+        public int MyProperty { get; set; }
+    }
+}
